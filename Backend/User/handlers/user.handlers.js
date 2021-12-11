d0dbc7dee7295faf526b9cec935188a64c7bc077
PatkarAditya22 <adityapatkar22@gmail.com>
2021-12-11 21:13:00 +0530

    Proposed Token route added

--- Backend/User/handlers/user.handlers.js
+++ Backend/User/handlers/user.handlers.js
@@ -1,6 +1,49 @@
 import db from "../models/index.js";
 
-const proposeToken = async (req, res, next) => {};
+const proposeToken = async (req, res, next) => {
+	try {
+		const {
+			email,
+			mobile,
+			name,
+			dateOfBirth,
+			image,
+			gender,
+			collegeInfo,
+			degreeOfPlay,
+			country,
+			ethereumAddress,
+			awardsAndAccolades,
+			certificates,
+		} = req.body;
+
+		const proposedToken = await db.ProposedToken.create({
+			email,
+			mobile,
+			name,
+			dateOfBirth,
+			image,
+			gender,
+			collegeInfo,
+			degreeOfPlay,
+			country,
+			ethereumAddress,
+			awardsAndAccolades,
+			certificates,
+		});
+
+		await db.User.findOneAndUpdate(
+			{ id: req.params.id },
+			{ $push: { tokens: proposedToken._id } }
+		);
+		res
+			.status(201)
+			.json({ proposedToken, message: "Token created and Proposed" });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: error.message });
+	}
+};
 
 const getTokenDetails = async (req, res, next) => {};
 
--- Backend/User/models/index.js
+++ Backend/User/models/index.js
@@ -1,19 +1,22 @@
 import mongoose from "mongoose";
 import User from "./User.models.js";
+import ProposedToken from "./ProposedTokens.models.js";
+
 mongoose.Promise = global.Promise;
 const databaseUri = process.env.MONGODB_URI || "mongodb://localhost/FYP";
 
 // Connect to MongoDB
 mongoose
-  .connect(databaseUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    keepAlive: true,
-  })
-  .then(() => console.log(`Database connected to ${databaseUri}`))
-  .catch((err) => console.log(`Database connection error: ${err.message}`));
+	.connect(databaseUri, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		keepAlive: true,
+	})
+	.then(() => console.log(`Database connected to ${databaseUri}`))
+	.catch((err) => console.log(`Database connection error: ${err.message}`));
 
 // Export all the models
 export default {
-  User
+	User,
+	ProposedToken,
 };
