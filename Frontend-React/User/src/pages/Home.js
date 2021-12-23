bad01c1577e3cc67cd57f25b17df71fea8002b26
Archeel Parekh <archeelparekh11@gmail.com>
2021-12-23 19:44:13 +0530

    Homepage minor font change

--- Frontend-React/User/src/pages/Home.js
+++ Frontend-React/User/src/pages/Home.js
@@ -68,7 +68,10 @@ const Home = ({ isAuthenticated, setIsAuthenticated }) => {
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Before they sold out
               <br className="hidden lg:inline-block" />readymade gluten
             </h1>
-            <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+            <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
+              fugiat veniam occaecat fugiat aliqua.
+            </p>
           </div>
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
             <img className="object-cover object-center rounded" alt="hero" src={PlayerLogo} />
