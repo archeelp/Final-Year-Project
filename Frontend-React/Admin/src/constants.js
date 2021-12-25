e15138ad4adaac5ec8c08f1a6232ec0b06c41373
PatkarAditya22 <adityapatkar22@gmail.com>
2021-12-25 12:10:31 +0530

    Admin side frontend initialized

---
+++ Frontend-React/Admin/src/constants.js
@@ -0,0 +1,3 @@
+export const baseURL = "https://final-year-project-aams.herokuapp.com";
+export const tokenContractAddress = "0x10E0b591e76EBA99899ae87feb051f588949aA79";
+export const oneETH = 1000000000000000000;
