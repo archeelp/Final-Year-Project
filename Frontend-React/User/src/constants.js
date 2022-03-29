a0bbcb90ec31311c58ae91e6ebd26e9fd5ba241e
ssrushti <srushti123shah@gmail.com>
2022-03-29 13:45:52 +0530

    "minor"

--- Frontend-React/User/src/constants.js
+++ Frontend-React/User/src/constants.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-export const baseURL = "https://khelofi-backend.herokuapp.com/";
-=======
+
 // export const baseURL = "https://final-year-project-aams.herokuapp.com";
 export const baseURL = "https://khelofi-backend.herokuapp.com";
->>>>>>> 474cb7d7248ee0ca3396365c4a408500b2ad9f35
 export const tokenContractAddress =
 	"0x6F1006eDA4386D1f1b83afBA7Fe9C66aBF2d52Ba";
 export const oneETH = 1000000000000000000;
